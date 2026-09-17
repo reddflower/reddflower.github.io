2812243adaf6866e7341d0e9577e30d7eb84882e
reddflower <bokhandanil09@gmail.com>
2026-09-17 23:19:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+﻿// Проверяем, есть ли старая кнопка 'myButton'
+const button = document.getElementById('myButton');
+if (button) {
+    button.addEventListener('click', function() {
+        alert('Ура! Ваш JavaScript код успешно работает!');
+    });
+}
+
+// Проверяем кнопку перехода на test.html
+const btnTest = document.getElementById('btnTest');
+if (btnTest) {
+    btnTest.addEventListener('click', function() {
+        window.location.href = '/test.html'; 
+    });
+}
+
+// Проверяем кнопку перехода в папку folder
+const btnFolderTest = document.getElementById('btnFolderTest');
+if (btnFolderTest) {
+    btnFolderTest.addEventListener('click', function() {
+        window.location.href = '/folder/test.html';
+    });
+}
+
+// Безопасная кнопка "Назад" для любой страницы
+const btnGoBack = document.getElementById('btnBack');
+if (btnGoBack) {
+    btnGoBack.addEventListener('click', function() {
+        window.history.back(); 
+    });
+}
